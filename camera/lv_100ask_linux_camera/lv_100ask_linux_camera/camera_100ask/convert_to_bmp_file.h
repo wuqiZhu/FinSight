4029f66135fd0e8cc308d58c06d2e8c6e8fa4703
Zhu Xiangbo <zhuxiangbo@example.com>
2026-06-18 17:38:48 +0800

    只保留核心项目

--- camera/lv_100ask_linux_camera/lv_100ask_linux_camera/camera_100ask/convert_to_bmp_file.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONVERT_TO_BMP_FILE_H
-#define CONVERT_TO_BMP_FILE_H
-
-#include "types.h"
-
-int CvtRgb2BMPFileFrmFrameBuffer(unsigned char * pRgb, UINT32 dwWidth, UINT32 dwHeight, UINT32 dwBpp, char *outfilename);
-
-#endif /*CONVERT_TO_BMP_FILE_H*/
